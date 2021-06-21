3a2948555043150491421cf3361abc1e860b26bd
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-06-21 11:53:42 +0530

    Add MovieCard & Navbar component with styling

---
+++ src/data.js
@@ -0,0 +1,202 @@
+export const data = [
+    {
+    "Title": "Superman1",
+    "Year": "1978",
+    "Rated": "PG",
+    "Released": "15 Dec 1978",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Richard Donner",
+    "Writer": "Mario Puzo (story), Mario Puzo (screenplay), David Newman (screenplay), Leslie Newman (screenplay), Robert Benton (screenplay)",
+    "Actors": "Marlon Brando, Gene Hackman, Christopher Reeve, Ned Beatty",
+    "Plot": "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    "Language": "English",
+    "Country": "USA, UK, Canada, Switzerland, France",
+    "Awards": "Nominated for 3 Oscars. Another 18 wins & 20 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA0YWMwMTUtMTVhNC00NjRkLWE2ZTgtOWEzNjJhYzNiMTlkXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.3/10"
+    },
+    {
+    "Source": "Rotten Tomatoes",
+    "Value": "94%"
+    },
+    {
+    "Source": "Metacritic",
+    "Value": "80/100"
+    }
+    ],
+    "Metascore": "80",
+    "imdbRating": "7.3",
+    "imdbVotes": "161,675",
+    "imdbID": "tt0078346",
+    "Type": "movie",
+    "DVD": "15 Aug 2008",
+    "BoxOffice": "$134,451,603",
+    "Production": "Dovemead Films",
+    "Website": "N/A",
+    "Response": "True"
+},
+{
+    "Title": "Superman2",
+    "Year": "1978",
+    "Rated": "PG",
+    "Released": "15 Dec 1978",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Richard Donner",
+    "Writer": "Mario Puzo (story), Mario Puzo (screenplay), David Newman (screenplay), Leslie Newman (screenplay), Robert Benton (screenplay)",
+    "Actors": "Marlon Brando, Gene Hackman, Christopher Reeve, Ned Beatty",
+    "Plot": "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    "Language": "English",
+    "Country": "USA, UK, Canada, Switzerland, France",
+    "Awards": "Nominated for 3 Oscars. Another 18 wins & 20 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA0YWMwMTUtMTVhNC00NjRkLWE2ZTgtOWEzNjJhYzNiMTlkXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.3/10"
+    },
+    {
+    "Source": "Rotten Tomatoes",
+    "Value": "94%"
+    },
+    {
+    "Source": "Metacritic",
+    "Value": "80/100"
+    }
+    ],
+    "Metascore": "80",
+    "imdbRating": "7.3",
+    "imdbVotes": "161,675",
+    "imdbID": "tt0078346",
+    "Type": "movie",
+    "DVD": "15 Aug 2008",
+    "BoxOffice": "$134,451,603",
+    "Production": "Dovemead Films",
+    "Website": "N/A",
+    "Response": "True"
+},
+{
+    "Title": "Superman3",
+    "Year": "1978",
+    "Rated": "PG",
+    "Released": "15 Dec 1978",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Richard Donner",
+    "Writer": "Mario Puzo (story), Mario Puzo (screenplay), David Newman (screenplay), Leslie Newman (screenplay), Robert Benton (screenplay)",
+    "Actors": "Marlon Brando, Gene Hackman, Christopher Reeve, Ned Beatty",
+    "Plot": "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    "Language": "English",
+    "Country": "USA, UK, Canada, Switzerland, France",
+    "Awards": "Nominated for 3 Oscars. Another 18 wins & 20 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA0YWMwMTUtMTVhNC00NjRkLWE2ZTgtOWEzNjJhYzNiMTlkXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.3/10"
+    },
+    {
+    "Source": "Rotten Tomatoes",
+    "Value": "94%"
+    },
+    {
+    "Source": "Metacritic",
+    "Value": "80/100"
+    }
+    ],
+    "Metascore": "80",
+    "imdbRating": "7.3",
+    "imdbVotes": "161,675",
+    "imdbID": "tt0078346",
+    "Type": "movie",
+    "DVD": "15 Aug 2008",
+    "BoxOffice": "$134,451,603",
+    "Production": "Dovemead Films",
+    "Website": "N/A",
+    "Response": "True"
+},
+{
+    "Title": "Superman4",
+    "Year": "1978",
+    "Rated": "PG",
+    "Released": "15 Dec 1978",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Richard Donner",
+    "Writer": "Mario Puzo (story), Mario Puzo (screenplay), David Newman (screenplay), Leslie Newman (screenplay), Robert Benton (screenplay)",
+    "Actors": "Marlon Brando, Gene Hackman, Christopher Reeve, Ned Beatty",
+    "Plot": "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    "Language": "English",
+    "Country": "USA, UK, Canada, Switzerland, France",
+    "Awards": "Nominated for 3 Oscars. Another 18 wins & 20 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA0YWMwMTUtMTVhNC00NjRkLWE2ZTgtOWEzNjJhYzNiMTlkXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.3/10"
+    },
+    {
+    "Source": "Rotten Tomatoes",
+    "Value": "94%"
+    },
+    {
+    "Source": "Metacritic",
+    "Value": "80/100"
+    }
+    ],
+    "Metascore": "80",
+    "imdbRating": "7.3",
+    "imdbVotes": "161,675",
+    "imdbID": "tt0078346",
+    "Type": "movie",
+    "DVD": "15 Aug 2008",
+    "BoxOffice": "$134,451,603",
+    "Production": "Dovemead Films",
+    "Website": "N/A",
+    "Response": "True"
+},
+{
+    "Title": "Superman5",
+    "Year": "1978",
+    "Rated": "PG",
+    "Released": "15 Dec 1978",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Richard Donner",
+    "Writer": "Mario Puzo (story), Mario Puzo (screenplay), David Newman (screenplay), Leslie Newman (screenplay), Robert Benton (screenplay)",
+    "Actors": "Marlon Brando, Gene Hackman, Christopher Reeve, Ned Beatty",
+    "Plot": "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    "Language": "English",
+    "Country": "USA, UK, Canada, Switzerland, France",
+    "Awards": "Nominated for 3 Oscars. Another 18 wins & 20 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA0YWMwMTUtMTVhNC00NjRkLWE2ZTgtOWEzNjJhYzNiMTlkXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.3/10"
+    },
+    {
+    "Source": "Rotten Tomatoes",
+    "Value": "94%"
+    },
+    {
+    "Source": "Metacritic",
+    "Value": "80/100"
+    }
+    ],
+    "Metascore": "80",
+    "imdbRating": "7.3",
+    "imdbVotes": "161,675",
+    "imdbID": "tt0078346",
+    "Type": "movie",
+    "DVD": "15 Aug 2008",
+    "BoxOffice": "$134,451,603",
+    "Production": "Dovemead Films",
+    "Website": "N/A",
+    "Response": "True"
+}]
+
