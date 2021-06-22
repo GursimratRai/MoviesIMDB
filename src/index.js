bcba418b8afff6b590498778318d59324fa70163
Gursimrat Rai <gursimratsinghrai@gmail.com>
2021-06-22 11:24:59 +0530

    Install Redux & set action,reducer,store for Redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './Components/App';
+import movies from './Reducers';
+
+const store = createStore(movies);
+console.log('before',store.getState());
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies: [{name:'Superman'}]
+});
+console.log('after',store);
 
 ReactDOM.render(
   <React.StrictMode>
